ad146b167cf636150298791e04478f217b119856
Gabu-Rayon <gibsonmurimi4@gmail.com>
2023-06-28 22:10:06 +0300

    commit

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use App\Imports\UsersImport;
 use Illuminate\Http\Request;
+use Maatwebsite\Excel\Facades\Excel;
 
 class UserController extends Controller
 {
@@ -11,5 +13,8 @@ class UserController extends Controller
         $users = User::get();
         return view('index',compact('users'));
         
+    }
+    public function import(){
+        Excel::import(new UsersImport,request()->file('file'));
     }
 }
--- app/Imports/UsersImport.php
+++ app/Imports/UsersImport.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Hash;
 use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
 
-class UserImport implements ToModel,WithHeadingRow
+class UsersImport implements ToModel,WithHeadingRow
 {
     /**
     * @param array $row to import data from excel
